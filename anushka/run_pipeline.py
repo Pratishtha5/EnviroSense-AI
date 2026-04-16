5d173c6e26d3abecd2cb4c9c2e8b1cbf1e952db6
anujain13 <anushkaj2100@gmail.com>
2026-04-16 19:12:24 +0530

    remove duplicate success print

--- anushka/run_pipeline.py
+++ anushka/run_pipeline.py
@@ -1,15 +1,10 @@
 import time
 from member4_model import run_pipeline
 
-
 def main():
     while True:
-        
         run_pipeline()
-            
-
         time.sleep(60)
-
 
 if __name__ == "__main__":
     main()
